7944b713863c643b3a9fc44172118efcfe219858
maekawa <kazumaekawa7218@gmail.com>
2020-12-28 20:15:29 +0900

    Create LikeHeader.vue & LikeNumber.vue

---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["headerText"],
+  data() {
+    return {
+      user: {
+        firstName: 'Jack',
+        lastName: 'Donald'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+
+  h1 {
+    color: red;
+  }
+</style>
